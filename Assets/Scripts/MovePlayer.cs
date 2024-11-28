184d55f752725f01448a1addfa4a38468afd3398
mrmin50000 <danilast2007@gmail.com>
2024-11-28 13:43:52 +0300

    Save scene

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -5,6 +5,7 @@ public class MovePlayer : MonoBehaviour
 
     private Rigidbody2D _rb;
     private float _speed = 4f;
+    private int k = 0;
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -20,9 +21,24 @@ public class MovePlayer : MonoBehaviour
         {
             _rb.linearVelocityX = -_speed;
         }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            k++;
+            _rb.linearVelocityY = _speed;
+            if(k > 2)
+            {
+                _rb.linearVelocityY = 0;
+            }
+        }
         else
         {
             _rb.linearVelocityX = 0f;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        k = 0;
+    }
+
 }

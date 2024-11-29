3c17460c77fa2737b9c12a4403ffd5d250d6a15f
BeJloHukku <igoryashamb@gmail.com>
2024-11-29 22:14:28 +0300

    Change the logic of flying (not ready)

--- Assets/Scripts/BirdFly.cs
+++ Assets/Scripts/BirdFly.cs
@@ -1,29 +1,75 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BirdFly : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rigitbody;
-    [SerializeField] Camera camera;
+
+    [SerializeField] private float _moveSpeed = 2;
+    [SerializeField] private Transform _center;
+    [SerializeField] private float _distToWall = 0.5f;
+    [SerializeField] private LayerMask _platformsLayer;
+
+    private Rigidbody2D _rb;
+    private float _startPositionX;
 
 
-    private float _speed = 5;
-    private int _counterOfFlyTrails = 0;
-    private float _cameraHight;
-    private float _cameraWidth;
-    void Update()
+    private Vector3 ForwardVector
+    {
+        get => (-transform.right * transform.lossyScale.x).normalized;
+    }
+
+    private Vector3 DownVector
+    {
+        get => (-transform.up.normalized * transform.lossyScale.y).normalized;
+    }
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        Collider2D collider = GetComponent<Collider2D>();
+        _startPositionX = transform.position.x;
+        Debug.Log($"Начальная позиция по X: {_startPositionX}");
+    }
+    private void Update()
+    {
+        transform.position += ForwardVector * _moveSpeed * Time.deltaTime;
+    }
+
+    private bool isDistant()
+    {
+        if (_startPositionX > 0) return Math.Abs(transform.localPosition.x) >= _startPositionX;
+        return transform.localPosition.x >= Math.Abs(_startPositionX);
+    }
+
+    private bool isCollisionWithWall()
+    {
+         return Physics2D.Raycast(_center.position, ForwardVector, _distToWall, _platformsLayer);
+    }
+
+    private bool isReadyToFlip()
+    {
+        return isDistant();
+    }
+
+    private void Flip()
+    {
+        float pivotDistance = -_center.localPosition.x;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        transform.position += -ForwardVector * (pivotDistance - 0.1f);
+    }
+
+
+    private void FixedUpdate()
     {
 
-        rigitbody.linearVelocity = new Vector2(-_speed, 0);
-        ////if (this.transform.position.x > _cameraWidth + 3)
-        ////{
-        ////    _counterOfFlyTrails++;
-        ////}
-        ////if (_counterOfFlyTrails > 5)
-        ////{
-        ////    Destroy(this);
-        ////}
-        //if (this.transform.position.x > -_cameraWidth + 3)
-        //    rigitbody.linearVelocity = new Vector2(0, 0);
+        Debug.Log(transform.localPosition.x);
+        if (isReadyToFlip())
+            Flip();
     }
+
+
+
+
+
 }

c338bf2fc672d6997c9915317652cf089ddd75b5
alexey <166526599+AlexeyAndreevDM@users.noreply.github.com>
2024-11-30 10:19:53 +0300

    Update JumpPlayer.cs

--- Assets/Scripts/JumpPlayer.cs
+++ Assets/Scripts/JumpPlayer.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class JumpPlayer : MonoBehaviour
 {
-
     [SerializeField] private LayerMask _platformsLayer;
     [SerializeField] private float _jumpForce = 5f;
-    [SerializeField] private float _rayDistance = 0.6f;
     private Rigidbody2D _rb;
     private bool _isGround;
     private bool _doubleJump = false;
@@ -16,32 +14,48 @@ public class JumpPlayer : MonoBehaviour
     }
 
     private void Update()
-    { 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && _isGround)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            if (_isGround)
+            {
+                Jump();
+            }
+            else if (!_doubleJump)
+            {
+                _doubleJump = true;
+                Jump();
+            }
         }
-        else if(!_doubleJump && Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            _doubleJump = true;
-            _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
-        }       
     }
 
-    private void FixedUpdate()
+    private void Jump()
     {
-        RaycastHit2D hit = Physics2D.Raycast(_rb.position, Vector2.down, _rayDistance, _platformsLayer);
-        if (hit.collider != null)
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0);
+        _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & _platformsLayer) != 0)
         {
-            _isGround = true;
-            _doubleJump = false;
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f)
+                {
+                    _isGround = true;
+                    _doubleJump = false;
+                    break;
+                }
+            }
         }
-        else
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & _platformsLayer) != 0)
+        {
             _isGround = false;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawRay(transform.position, Vector2.down * _rayDistance);
+        }
     }
 }

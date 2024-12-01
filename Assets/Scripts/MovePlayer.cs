218cc1b1be1ec05a4568b29a602f30a63fca53f1
kroften <sskozhemyakin2006@gmail.com>
2024-12-01 14:35:15 +0300

    Add wall jump pushing

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 4;
-    [SerializeField] private float _jumpForce = 12.5f;
+    [SerializeField] private float _jumpStrength = 12.5f;
     [SerializeField] private float _maxJumpAmount = 2;
+    [SerializeField] private float _wallJumpStrength = 5;
+    [SerializeField] private float _wallJumpTime = 0.4f;
+    [SerializeField] private float _wallJumpTimer = 0;
+    [SerializeField] private bool _isWallJumping = false;
+    [SerializeField] private int _wallJumpDirection;
     private Vector2 _input;
     private Rigidbody2D _rb;
     private float _borderOffsetX;
@@ -44,39 +49,73 @@ public class MovePlayer : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (IsGrounded())
+        bool isGrounded = IsGrounded();
+        if (isGrounded)
             _timesJumped = 0;
 
         _rb.linearVelocityX = _input.x * _moveSpeed;
 
-        if (_input.y == 1 && _timesJumped < _maxJumpAmount)
+        if (_input.y == 1)
         {
-            _rb.linearVelocityY = 0;
-            _rb.AddForceY(_jumpForce, ForceMode2D.Impulse);
-            if (!IsWallOnLeft() && !IsWallOnRight())
+            int wallDirection = GetWallDirection();
+            if (wallDirection != 0 && !isGrounded)
+            {
+                WallJump(wallDirection);
+            }
+            else if (_timesJumped < _maxJumpAmount)
+            {
                 _timesJumped++;
+                _rb.linearVelocityY = 0;
+                _rb.AddForceY(_jumpStrength, ForceMode2D.Impulse);
+            }
+
             _input.y = 0;
+        }
+
+
+    }
+
+    private void WallJump(int wallDirection)
+    {
+        _wallJumpTimer = 0;
+        _wallJumpDirection = -wallDirection;
+        _isWallJumping = true;
+        _rb.linearVelocityY = 0;
+        _rb.AddForceY(_jumpStrength, ForceMode2D.Impulse);
+
+        if (_isWallJumping)
+        {
+            _rb.linearVelocityX = _wallJumpDirection * _wallJumpStrength;
+            _wallJumpTimer += Time.fixedDeltaTime;
+            if (_wallJumpTimer >= _wallJumpTime)
+            {
+                _isWallJumping = false;
+                _wallJumpTimer = 0;
+            }
         }
     }
 
-    private bool IsWallOnLeft()
+    private int GetWallDirection()
     {
-        return Physics2D.Raycast(
+        bool leftWall = Physics2D.Raycast(
             new Vector2(transform.position.x - _borderOffsetX, transform.position.y),
             Vector2.left,
             _wallRaycastLength,
             _groundLayerMask
         );
-    }
-
-    private bool IsWallOnRight()
-    {
-        return Physics2D.Raycast(
-            new Vector2(transform.position.x + _borderOffsetX, transform.position.y), 
-            Vector2.right,
-            _wallRaycastLength,
-            _groundLayerMask
+        bool rightWall = Physics2D.Raycast(
+          new Vector2(transform.position.x + _borderOffsetX, transform.position.y),
+          Vector2.right,
+          _wallRaycastLength,
+          _groundLayerMask
         );
+
+
+        if (leftWall)
+            return -1;
+        if (rightWall)
+            return 1;
+        return 0;
     }
 
     private bool IsGrounded()

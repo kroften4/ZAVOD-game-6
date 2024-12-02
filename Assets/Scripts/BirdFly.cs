cc6b7821aa21c39c06e01fafcc1cf8104292c5c4
BeJloHukku <igoryashamb@gmail.com>
2024-12-02 13:26:20 +0300

    ReadyBird

--- Assets/Scripts/BirdFly.cs
+++ Assets/Scripts/BirdFly.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,8 +8,9 @@ public class BirdFly : MonoBehaviour
 
     [SerializeField] private float _moveSpeed = 2;
     [SerializeField] private Transform _center;
-    [SerializeField] private float _distToWall = 0.5f;
+    [SerializeField] private float _distToWall = 1.0f;
     [SerializeField] private LayerMask _platformsLayer;
+    [SerializeField] private float _flyingDistance = 20f;
 
     private Rigidbody2D _rb;
     private float _startPositionX;
@@ -19,52 +21,45 @@ public class BirdFly : MonoBehaviour
         get => (-transform.right * transform.lossyScale.x).normalized;
     }
 
-    private Vector3 DownVector
-    {
-        get => (-transform.up.normalized * transform.lossyScale.y).normalized;
-    }
+
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         Collider2D collider = GetComponent<Collider2D>();
         _startPositionX = transform.position.x;
-        Debug.Log($"Начальная позиция по X: {_startPositionX}");
     }
     private void Update()
     {
         transform.position += ForwardVector * _moveSpeed * Time.deltaTime;
     }
 
-    private bool isDistant()
+    private bool IsDistant()
     {
-        if (_startPositionX > 0) return Math.Abs(transform.localPosition.x) > _startPositionX;
-        return transform.localPosition.x > Math.Abs(_startPositionX);
+        return Math.Abs(transform.position.x - _startPositionX) > _flyingDistance;
     }
 
-    private bool isCollisionWithWall()
+    private bool IsCollisionWithWall()
     {
-         return Physics2D.Raycast(_center.position, ForwardVector, _distToWall, _platformsLayer);
+        return Physics2D.Raycast(_center.position, ForwardVector, _distToWall, _platformsLayer);
     }
 
-    private bool isReadyToFlip()
-    {
-        return isDistant() || isCollisionWithWall();
-    }
+    
 
     private void Flip()
     {
-        float pivotDistance = -_center.localPosition.x;
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
     }
 
 
     private void FixedUpdate()
     {
-
-        Debug.Log(transform.localPosition.x);
-        if (isReadyToFlip())
+        if (IsDistant() || IsCollisionWithWall())
+        {
             Flip();
+            _startPositionX = transform.position.x;
+        }
     }
 
 

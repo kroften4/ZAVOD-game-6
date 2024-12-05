d1ca69266baa828f1f25a19783cdd2e8b923e2e8
mrmin50000 <danilast2007@gmail.com>
2024-12-05 15:22:07 +0300

    fixed killing

--- Assets/Scripts/KillMobs.cs
+++ Assets/Scripts/KillMobs.cs
@@ -9,21 +9,23 @@ public class KillMobs : MonoBehaviour
     private Rigidbody2D _playerRb;
     private float _mobOffset;
     private Collider2D _col;
-    [SerializeField] private float _heightForKillMobs = 0.3f;
+    [SerializeField] private float _mobOffsetCoef = 0.5f;
     [SerializeField] private string _playerTag = "Player";
     [SerializeField] private float _forceValue = 15f;
     void Awake()
     {
         GameObject _player = GameObject.FindWithTag(_playerTag);
         _col = GetComponent<Collider2D>();
-        _mobOffset = _col.bounds.extents.y - _heightForKillMobs; 
+        _mobOffset = _col.bounds.extents.y * _mobOffsetCoef; 
         _playerRb = _player.GetComponent<Rigidbody2D>();
         _playerFeetOffset = _player.GetComponent<Collider2D>().bounds.extents.y;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.transform.position.y - _playerFeetOffset > transform.TransformPoint(_col.bounds.center).y  + _mobOffset && _playerRb.linearVelocityY < 0 && other.CompareTag(_playerTag))
+        Debug.Log($"_mobOffset {_mobOffset}, transform.TransformPoint(_col.bounds.center).y {transform.TransformPoint(_col.bounds.center).y}, other.transform.position.y - _playerFeetOffset {other.transform.position.y - _playerFeetOffset}, _col.bounds.center {_col.bounds.center}");
+
+        if (other.transform.position.y - _playerFeetOffset > _col.bounds.center.y + _mobOffset && _playerRb.linearVelocityY < 0 && other.CompareTag(_playerTag))
         {
             Destroy(gameObject);
             _playerRb.AddForceY(_forceValue, ForceMode2D.Impulse);

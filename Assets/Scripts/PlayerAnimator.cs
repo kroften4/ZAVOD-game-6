b36dbba47cafadb7454f0a2053bc000967742134
kroften <sskozhemyakin2006@gmail.com>
2024-12-09 22:45:51 +0300

    Add flipping for left-right movement

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -3,12 +3,13 @@ using UnityEngine.InputSystem;
 using UnityEngine.Windows;
 
 [RequireComponent(typeof(Animator), typeof(Rigidbody2D), typeof(GroundChecker))]
-[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerInput), typeof(SpriteRenderer))]
 public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
     private Rigidbody2D _rb;
     private GroundChecker _groundChecker;
+    private SpriteRenderer _spriteRenderer;
     private Vector2 _linearVelocity;
     private bool _isHuggingWall;
     private float _xInput;
@@ -18,6 +19,7 @@ public class PlayerAnimator : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _groundChecker = GetComponent<GroundChecker>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnMovement(InputValue value)
@@ -29,24 +31,25 @@ public class PlayerAnimator : MonoBehaviour
     {
         _animator.SetFloat("VelocityY", _linearVelocity.y);
         _animator.SetBool("IsWalled", _isHuggingWall);
-    }
-
-    private void FixedUpdate()
-    {
         switch (_xInput)
         {
             case -1:
                 _animator.SetBool("LeftKey", true);
+                _spriteRenderer.flipX = true;
                 break;
             case 1:
                 _animator.SetBool("RightKey", true);
+                _spriteRenderer.flipX = false;
                 break;
             case 0:
                 _animator.SetBool("LeftKey", false);
                 _animator.SetBool("RightKey", false);
                 break;
         }
+    }
 
+    private void FixedUpdate()
+    {
         _isHuggingWall = _groundChecker.GetWallDirection() != 0 && !_groundChecker.IsGrounded();
         _linearVelocity = _rb.linearVelocity;
     }

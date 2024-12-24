2ff7f080b5b441a67d1d471182b8866bec5210c7
alexey <166526599+AlexeyAndreevDM@users.noreply.github.com>
2024-12-24 18:18:26 +0300

    wind particles

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -11,11 +11,28 @@ public class Wind : MonoBehaviour
 
     AudioManager _audioManager;
 
+    public GameObject _windPrefab;
+    public GameObject _windStraightPrefab;
+
+    private GameObject _wind;
+    private GameObject _windStraight;
+
+    private ParticleSystem _windParticleSystem;
+    private ParticleSystem _windStraightParticleSystem;
+
+    private Vector2 offset = new Vector2(1f, 0f);
+
     void Start()
     {
         _rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         _audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-        
+
+        _wind = Instantiate(_windPrefab, transform.position, Quaternion.identity);
+        _windStraight = Instantiate(_windStraightPrefab, transform.position, Quaternion.identity);
+
+        _windParticleSystem = _wind.GetComponent<ParticleSystem>();
+        _windStraightParticleSystem = _windStraight.GetComponent<ParticleSystem>();
+
         StartCoroutine(WindCoroutine());
     }
 
@@ -23,17 +40,42 @@ public class Wind : MonoBehaviour
     {
         while (true)
         {
-            float windDuration = Random.Range(3f, 30f);
+            float _windDuration = Random.Range(3f, 30f);
             float angle = Random.Range(0f, 360f);
             _windDirection = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Random.value > 0.5f ? Vector2.up.y : Vector2.down.y).normalized;
             _isWinding = true;
+
+            if (_windParticleSystem != null)
+            {
+                _windParticleSystem.Play();
+                SetParticleVisibility(_windParticleSystem, true);
+            }
+            if (_windStraightParticleSystem != null)
+            {
+                _windStraightParticleSystem.Play();
+                SetParticleVisibility(_windStraightParticleSystem, true);
+            }
+
+            UpdateWindParticles();
+
             if (_audioManager._wind.Length > 0)
             {
                 int randomIndex = Random.Range(0, _audioManager._wind.Length);
                 _audioManager.SFXSource.PlayOneShot(_audioManager._wind[randomIndex]);
             }
 
-            yield return new WaitForSeconds(windDuration);
+            yield return new WaitForSeconds(_windDuration);
+
+            if (_windParticleSystem != null)
+            {
+                _windParticleSystem.Stop();
+                SetParticleVisibility(_windParticleSystem, false);
+            }
+            if (_windStraightParticleSystem != null)
+            {
+                _windStraightParticleSystem.Stop();
+                SetParticleVisibility(_windStraightParticleSystem, false);
+            }
 
             _isWinding = false;
             float calmDuration = Random.Range(3f, 30f);
@@ -47,5 +89,25 @@ public class Wind : MonoBehaviour
         {
             _rb.AddForce(_windDirection * _windStrength);
         }
+    }
+
+    private void UpdateWindParticles()
+    {
+        if (_wind != null)
+        {
+            _wind.transform.position = (Vector2)_rb.position + offset;
+            _wind.transform.up = _windDirection;
+        }
+
+        if (_windStraight != null)
+        {
+            _windStraight.transform.position = (Vector2)_rb.position + offset;
+            _windStraight.transform.up = _windDirection;
+        }
+    }
+
+    private void SetParticleVisibility(ParticleSystem _ps, bool isVisible)
+    {
+        _ps.gameObject.SetActive(isVisible);
     }
 }
